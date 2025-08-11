8eda14fb4d2207d297b1a8e6927b198feb360f6f
xeuc <80948867+xeuc@users.noreply.github.com>
2025-08-11 18:14:42 +0200

    reproduced shaking on minimal exemple

--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,365 @@
-use bevy::prelude::*;
-use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
-// use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin}, input::{mouse::MouseMotion, InputSystem}, prelude::*
+};
+use bevy_rapier3d::{control::KinematicCharacterController, prelude::*};
+use bevy::{
+    render::camera::Viewport, window::WindowResized,
+};
 
-pub mod plugins;
 
-#[derive(Component)]
-struct Player;
+mod spawners;
+use spawners::{
+    CameraPosition, RotateCamera, TopLeftCamera, Direction,
+};
+
+const MOUSE_SENSITIVITY: f32 = 0.3;
+const GROUND_TIMER: f32 = 0.5;
+const MOVEMENT_SPEED: f32 = 8.0;
+const JUMP_SPEED: f32 = 20.0;
+const GRAVITY: f32 = -9.81;
 
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::srgb(
+            0xF9 as f32 / 255.0,
+            0xF9 as f32 / 255.0,
+            0xFF as f32 / 255.0,
+        )))
+        .init_resource::<MovementInput>()
+        .init_resource::<LookInput>()
         .add_plugins((
             DefaultPlugins,
-            FrameTimeDiagnosticsPlugin,
-            // WorldInspectorPlugin::new(),
+            RapierPhysicsPlugin::<NoUserData>::default(),
+            RapierDebugRenderPlugin::default(),
+            
+            // The other diagnostics plugins can still be used without this if you want to use them in an ingame overlay for example.
+            LogDiagnosticsPlugin::default(),
+            // Adds frame time, FPS and frame count diagnostics.
+            FrameTimeDiagnosticsPlugin::default(),
+            // Adds an entity count diagnostic.
+            bevy::diagnostic::EntityCountDiagnosticsPlugin,
+            // Adds cpu and memory usage diagnostics for systems and the entire game process.
+            bevy::diagnostic::SystemInformationDiagnosticsPlugin,
+            // Forwards various diagnostics from the render app to the main app.
+            // These are pretty verbose but can be useful to pinpoint performance issues.
+            // bevy_render::diagnostic::RenderDiagnosticsPlugin,
+
+            
         ))
-        .add_plugins((
-            plugins::lost_player::LostPlayerPlugin,
-            plugins::screenshot::ScreenshotPlugin,
-            plugins::cursor_as_movement::RotateHeadPlugin,
-            plugins::pause::PausePlugin,
-            plugins::keyboard::KeyboardPlugin,
-            plugins::skybox::SkyboxPlugin,
-            plugins::light::LightPlugin,
-            // plugins::fps::FpsPlugin,
-            // plugins::egui::UiPlugin,
-            plugins::app_state::AppStatePlugin,
-            plugins::setup_map::SetupMapPlugin,
-            plugins::player::spawn_player_ball::SpawnPlayerBallPlugin,
-            // plugins::debug::DebugPlugin,
-            plugins::pokeball::SpawnPokeBallPlugin,
-        ))
-        .add_plugins((
-            plugins::animation::AnimationPlugin,
-            plugins::player::controls_player_ball::ControlsPlayerBallPlugin,
-            plugins::iyes_perf_ui::IvesPerfUIPlugin,
+        .add_systems(Startup, (
+            spawners::setup_player_camera_integrated,
+            spawners::setup_maps_elements,
+            spawners::setup_ui,
+            spawners::cursor_grab,
         ))
 
-        // .insert_resource(TimestepMode::Fixed {
-        //     dt: 1.0 / 240.0,
-        //     substeps: 64,
-        // })// Clip using "f" key
+        .add_systems(PreUpdate, handle_input.after(InputSystem))
+        .add_systems(FixedUpdate, translate_player)
+        // .add_systems(FixedUpdate, translate_cam.after(translate_player))
+        .add_systems(Update, (
+            rotate_player_and_cam,
+            rotate_button_behavior,
+            resize_little_rectangle,
+        ))
+
         .run();
 }
 
-// TODO
-// BUG
-// * Plugins should not call each other, but rather use queries (e.g., skybox is called by setup_maps)
-// Fix "ERROR present_frames: log: No work has been submitted for this frame" when taking a screenshot (to be continued)
 
-// MAP
-// Import camera as glTF
-// Import light as glTF
-// Save glTF modified by game map
-// Blender same dynamic scene https://www.youtube.com/watch?v=PRDUH0JUS_A
-// [OPTI] Do not see what is behind you
+/// Keyboard input vector
+#[derive(Default, Resource, Deref, DerefMut)]
+struct MovementInput(Vec3);
 
-// FEATURE
-// Config file
-// Split screen
-// Threads?
-// Portal-like realistic Nether portal
-// Dimension
-
-// COLLISION
-// Instead of using collision groups, just disable those that are not in the chunk
-// Please fix the slippery character
-// Rapier https://rapier.rs/docs/user_guides/bevy_plugin/getting_started_bevy/
-
-    // DONE
-    // Import glTF background (glTF file itself does not directly support a background)
-    // Finish the tutorial (???)
-    // Change everything to plugins
-    // Create a plugin for setup so that main is only 100 lines :)
-    // y < 1 => TP player
-    // Remove public (pub) from plugins
-    // UI log position + DEBUG
-    // Please put the light back
-    // Put UI back but as a plugin
-    // Fix screenshot folder not existing error
-    // [OPTI] MAKE check_mesh_ready_no_rapier CALLED ONCE (or not after the thing is done) (gamestate or component)
-    // Resolve light problem
-    // Generate trees procedurally? (No)
-
-// https://github.com/bevyengine/bevy/discussions/5522
-// egui is an immediate mode UI library.
-// This means that the state of the UI is built from scratch every frame,
-// but it also means the state is entirely transparent, without the need for propagating state through a retained tree.
-// In Bevy, using bevy_egui, this state is accessed through the EguiContext resource - a single struct that contains the entire UI state.
-// When you have mutable access to the EguiContext, you can add widgets, updating the state.
-
-// Create INDEPENDENT plugins or look for plugin dependencies
-
-// Performance:
-//           | ⧖ debug plugin | ϟ no debug plugin |
-// ⧖ no opti |         10 fps |           100 fps |
-// ϟ -o3     |        140 fps |           140 fps |
-
-// Change map
-
-// we should not draw polygons whose normals match the camera's view direction.
+/// Mouse input vector
+#[derive(Default, Resource, Deref, DerefMut)]
+struct LookInput(Vec2);
 
 
-// Create a Blockbench plugin to allow "extras" like Blender do with "custom properties" that can be reached from bevy using GltfExtras
-// camera should turn arond player without player turning as well 
 
-// Build for android is impossible (now)
 
-// I need a way to avoid using the mouse (for multi-screen).
-// I need controller support.
-// I need dynamic player addition by pressing a key.
-// I need a client-server implementation.
-// I need a way to switch between security and performance. (for server)
-// I need camera collision handling.
-// I need to manage other players, including non-local ones.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// keyboard  => movement_input: ResMut<MovementInput> => for translate_player_and_cam()
+// mouse pos => look_input:     ResMut<LookInput>     => for rotate_player_and_cam()
+fn handle_input(
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mut movement_input: ResMut<MovementInput>,
+    mut look_input: ResMut<LookInput>,
+    mut mouse_events: EventReader<MouseMotion>,
+    mut app_exit_events: EventWriter<AppExit>,
+) {
+    if keyboard.pressed(KeyCode::KeyW) {
+        movement_input.z -= 1.0;
+    }
+    if keyboard.pressed(KeyCode::KeyS) {
+        movement_input.z += 1.0
+    }
+    if keyboard.pressed(KeyCode::KeyA) {
+        movement_input.x -= 1.0;
+    }
+    if keyboard.pressed(KeyCode::KeyD) {
+        movement_input.x += 1.0
+    }
+    **movement_input = movement_input.normalize_or_zero();
+    if keyboard.pressed(KeyCode::Space) {
+        **movement_input *= 2.0;
+    }
+    if keyboard.pressed(KeyCode::ShiftLeft) {
+        movement_input.y = 1.0;
+    }
+
+    if keyboard.pressed(KeyCode::Escape) {
+        app_exit_events.write(AppExit::Success);
+    }
+
+    for event in mouse_events.read() {
+        look_input.x -= event.delta.x * MOUSE_SENSITIVITY;
+        look_input.y -= event.delta.y * MOUSE_SENSITIVITY;
+        look_input.y = look_input.y.clamp(-89.9, 89.9); // Limit pitch
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ACTUALLY TRANSLATE THE PLAYER AND THE CAMERA (6 directions)
+// no cam translate anymore
+// movement_input (keyboard)
+fn translate_player(
+    time: Res<Time>,
+    mut movement_input: ResMut<MovementInput>,
+    mut player: Query<(
+        &mut Transform,
+        &mut KinematicCharacterController,
+        Option<&KinematicCharacterControllerOutput>,
+    ), Without<Camera>>,
+    mut vertical_movement: Local<f32>,
+    mut grounded_timer: Local<f32>,
+) {
+    // let mut counter = 0; // first is cam, second is player
+    for (player_transform, mut player_controller, player_output) in player.iter_mut() {
+        // print!("------------------------------------------------\n");
+        // counter += 1;
+        // print!("counter: {:?}\n", counter);
+        // print!("player_transform: {:?}\n", player_transform);
+        // print!("player_controller: {:?}\n", player_controller);
+        // print!("player_output: {:?}\n", player_output);
+
+        // print!("movement_input: {:?}\n", movement_input[0]);
+        let delta_time = time.delta_secs();
+        // Retrieve input
+        let mut player_movement = Vec3::new(movement_input.x, 0.0, movement_input.z) * MOVEMENT_SPEED;
+        // print!("player_movement: {:?}\n", player_movement);
+        let jump_speed = movement_input.y * JUMP_SPEED;
+        // Clear input
+        // if counter == 2 {
+            **movement_input = Vec3::ZERO;
+        // }
+        // Check physics ground check
+        if player_output.map(|o| o.grounded).unwrap_or(false) {
+            *grounded_timer = GROUND_TIMER;
+            *vertical_movement = 0.0;
+        }
+        // If we are grounded we can jump
+        if *grounded_timer > 0.0 {
+            *grounded_timer -= delta_time;
+            // If we jump we clear the grounded tolerance
+            if jump_speed > 0.0 {
+                *vertical_movement = jump_speed;
+                *grounded_timer = 0.0;
+            }
+        }
+        player_movement.y = *vertical_movement;
+        *vertical_movement += GRAVITY * delta_time * player_controller.custom_mass.unwrap_or(1.0);
+        player_controller.translation = Some(player_transform.rotation * (player_movement * delta_time));
+
+    }
+
+}
+
+// ACTUALLY TRANSLATE THE PLAYER AND THE CAMERA (6 directions)
+// no cam translate anymore
+// movement_input (keyboard)
+fn _translate_cam(
+    time: Res<Time>,
+    mut movement_input: ResMut<MovementInput>,
+    mut player: Query<(
+        &mut Transform,
+        &mut KinematicCharacterController,
+        Option<&KinematicCharacterControllerOutput>,
+    ), (With<Camera3d>, Without<TopLeftCamera>)>,
+    mut vertical_movement: Local<f32>,
+    mut grounded_timer: Local<f32>,
+) {
+    // let mut counter = 0; // first is cam, second is player
+    for (player_transform, mut player_controller, player_output) in player.iter_mut() {
+        // print!("------------------------------------------------\n");
+        // counter += 1;
+        // print!("counter: {:?}\n", counter);
+        // print!("player_transform: {:?}\n", player_transform);
+        // print!("player_controller: {:?}\n", player_controller);
+        // print!("player_output: {:?}\n", player_output);
+
+        // print!("movement_input: {:?}\n", movement_input[0]);
+        let delta_time = time.delta_secs();
+        // Retrieve input
+        let mut player_movement = Vec3::new(movement_input.x, 0.0, movement_input.z) * MOVEMENT_SPEED;
+        // print!("player_movement: {:?}\n", player_movement);
+        let jump_speed = movement_input.y * JUMP_SPEED;
+        // Clear input
+        // if counter == 2 {
+            **movement_input = Vec3::ZERO;
+        // }
+        // Check physics ground check
+        if player_output.map(|o| o.grounded).unwrap_or(false) {
+            *grounded_timer = GROUND_TIMER;
+            *vertical_movement = 0.0;
+        }
+        // If we are grounded we can jump
+        if *grounded_timer > 0.0 {
+            *grounded_timer -= delta_time;
+            // If we jump we clear the grounded tolerance
+            if jump_speed > 0.0 {
+                *vertical_movement = jump_speed;
+                *grounded_timer = 0.0;
+            }
+        }
+        player_movement.y = *vertical_movement;
+        *vertical_movement += GRAVITY * delta_time * player_controller.custom_mass.unwrap_or(1.0);
+        player_controller.translation = Some(player_transform.rotation * (player_movement * delta_time));
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ACTUALLY ROTATE THE PLAYER (left and right) AND CAMERA (all 4 directions)
+// btw should not code for Cam as Cam = child(Player)
+//   => Actually no, it's for the cam to say "yes" (up and down)
+// look_input (mouse)
+fn rotate_player_and_cam(
+    mut player: Query<&mut Transform, (With<KinematicCharacterController>, Without<Camera>)>,
+    mut camera: Query<&mut Transform, (With<Camera>, Without<TopLeftCamera>)>,
+    look_input: Res<LookInput>,
+) {
+    let Ok(mut player_transform) = player.single_mut() else { return; };
+    player_transform.rotation = Quat::from_axis_angle(Vec3::Y, look_input.x.to_radians());
+    let Ok(mut camera_transform) = camera.single_mut() else { return;};
+    // camera_transform.rotation = Quat::from_axis_angle(Vec3::X, look_input.y.to_radians());
+    camera_transform.rotation = Quat::from_axis_angle(Vec3::Y, look_input.x.to_radians()) *
+        Quat::from_axis_angle(Vec3::X, look_input.y.to_radians());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Osef
+// Rotate button behavior
+fn rotate_button_behavior(
+    interaction_query: Query<
+        (&Interaction, &ComputedNodeTarget, &RotateCamera),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut camera_query: Query<&mut Transform, With<Camera>>,
+) {
+    for (interaction, computed_target, RotateCamera(direction)) in &interaction_query {
+        if let Interaction::Pressed = *interaction {
+            // Since TargetCamera propagates to the children, we can use it to find
+            // which side of the screen the button is on.
+            if let Some(mut camera_transform) = computed_target
+                .camera()
+                .and_then(|camera| camera_query.get_mut(camera).ok())
+            {
+                let angle = match direction {
+                    Direction::Left => -0.1,
+                    Direction::Right => 0.1,
+                };
+                camera_transform.rotate_around(Vec3::ZERO, Quat::from_axis_angle(Vec3::Y, angle));
+            }
+        }
+    }
+}
+
+
+
+// Osef
+// If windows resize => Then update little rectangle
+fn resize_little_rectangle(
+    windows: Query<&Window>,
+    mut resize_events: EventReader<WindowResized>,
+    mut query: Query<(&CameraPosition, &mut Camera), With<TopLeftCamera>>,
+) {
+    // We need to dynamically resize the camera's viewports whenever the window size changes
+    // so then each camera always takes up half the screen.
+    // A resize_event is sent when the window is first created, allowing us to reuse this system for initial setup.
+    for resize_event in resize_events.read() {
+        let window = windows.get(resize_event.window).unwrap();
+        let size = window.physical_size()/4 ;
+        // let size = UVec2::new(size.x/2 as u32, size.y as u32);
+
+        for (camera_position, mut camera) in &mut query {
+            camera.viewport = Some(Viewport {
+                physical_position: camera_position.pos * size,
+                physical_size: size,
+                ..default()
+            });
+        }
+    }
+}
---
+++ src/spawners.rs
@@ -0,0 +1,326 @@
+use bevy::window::{CursorGrabMode, PrimaryWindow};
+use bevy::{prelude::*, pbr::CascadeShadowConfigBuilder};
+use bevy_rapier3d::{control::KinematicCharacterController, prelude::*};
+use std::f32::consts::PI;
+
+
+
+
+/// ******************************************************************************
+/// ***  MISC  *******************************************************************
+/// ******************************************************************************
+
+#[derive(Component)]
+pub struct CameraPosition {
+    pub pos: UVec2,
+}
+#[derive(Component)]
+pub struct RotateCamera(pub Direction);
+
+#[derive(Component)]
+pub struct TopLeftCamera;
+
+pub enum Direction {
+    Left,
+    Right,
+}
+
+// make cursor don't move and invisible
+pub fn cursor_grab( 
+    mut q_windows: Query<&mut Window, With<PrimaryWindow>>,
+) {
+    let Ok(mut primary_window) = q_windows.single_mut() else { return;};
+    // primary_window.cursor_options.grab_mode = CursorGrabMode::Confined;
+    primary_window.cursor_options.grab_mode = CursorGrabMode::Locked;
+    primary_window.cursor_options.visible = false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/// ******************************************************************************
+/// ***  PLAYER + CAM0 ***********************************************************
+/// ******************************************************************************
+
+// Setup player entity and its integrated camera
+pub fn setup_player_camera_integrated(mut commands: Commands) {
+    
+    // Player
+    let player_entity = commands.spawn((
+        Transform::from_xyz(0.0, 5.0, 0.0),
+        Visibility::default(),
+        Collider::round_cylinder(0.9, 0.3, 0.2),
+        KinematicCharacterController {
+            custom_mass: Some(5.0),
+            up: Vec3::Y,
+            offset: CharacterLength::Absolute(0.01),
+            slide: true,
+            autostep: Some(CharacterAutostep {
+                max_height: CharacterLength::Relative(0.3),
+                min_width: CharacterLength::Relative(0.5),
+                include_dynamic_bodies: false,
+            }),
+            // Don’t allow climbing slopes larger than 45 degrees.
+            max_slope_climb_angle: 45.0_f32.to_radians(),
+            // Automatically slide down on slopes smaller than 30 degrees.
+            min_slope_slide_angle: 30.0_f32.to_radians(),
+            apply_impulse_to_dynamic_bodies: true,
+            snap_to_ground: None,
+            ..default()
+        },
+    )).id();
+
+    
+    // FPS Camera
+    let index = 0; // this is the player 2, 3, and 4 Comented out later in the code
+    let joint = SphericalJointBuilder::new()
+        .local_anchor1(Vec3::new(0.0, 0.0, 0.0))
+        .local_anchor2(Vec3::new(0.0, 0.0, 0.0));
+    commands.spawn((
+        Camera3d::default(), 
+        Transform::from_xyz(0.0, 0.2, 5.0).looking_at((0.0, 5.0, 0.0).into(), Vec3::Y),
+        Camera {
+            // Renders cameras with different priorities to prevent ambiguities
+            order: index as isize,
+            ..default()
+        },
+        CameraPosition {
+            pos: UVec2::new((index % 2) as u32, (index / 2) as u32),
+        },
+        // I added that
+        Collider::round_cylinder(0.3, 0.3, 0.3),
+        // KinematicCharacterController {
+        //     // custom_mass: Some(5.0),
+        //     up: Vec3::Y,
+        //     offset: CharacterLength::Absolute(0.01),
+        //     slide: true,
+        //     autostep: Some(CharacterAutostep {
+        //         max_height: CharacterLength::Relative(0.3),
+        //         min_width: CharacterLength::Relative(0.5),
+        //         include_dynamic_bodies: false,
+        //     }),
+        //     // Don’t allow climbing slopes larger than 45 degrees.
+        //     max_slope_climb_angle: 45.0_f32.to_radians(),
+        //     // Automatically slide down on slopes smaller than 30 degrees.
+        //     min_slope_slide_angle: 30.0_f32.to_radians(),
+        //     apply_impulse_to_dynamic_bodies: true,
+        //     snap_to_ground: None,
+        //     ..default()
+        // },
+        RigidBody::Dynamic,
+        ImpulseJoint::new(player_entity, joint)
+    ));
+
+    
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/// ******************************************************************************
+/// ***  MAP  ********************************************************************
+/// ******************************************************************************
+
+pub fn setup_maps_elements(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Ground
+    let ground_size = 50.0;
+    let ground_height = 0.1;
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(2.0*ground_size, 2.0*ground_height, 2.0*ground_size))),
+        // MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
+        MeshMaterial3d(materials.add(Color::srgb(0.3, 0.5, 0.3))),
+        Transform::from_xyz(0.0, -ground_height, 0.0),
+        Collider::cuboid(ground_size, ground_height, ground_size),
+    ));
+
+
+
+    // Stairs-Wall
+    let stair_len = 30;
+    let stair_step = 0.2;
+    for i in 1..=stair_len {
+        let step = i as f32;
+        let collider = Collider::cuboid(1.0, step * stair_step, 1.0);
+        let cuboid = Mesh3d(meshes.add(Cuboid::new(2.0*1.0, 2.0*step * stair_step, 2.0*1.0)));
+        let material= MeshMaterial3d(materials.add(Color::srgb(153.0/255.0, 90.0/255.0, 50.0/255.0)));
+        commands.spawn((
+            Transform::from_xyz(40.0, step * stair_step, step * 2.0 - 20.0),
+            cuboid.clone(),
+            material.clone(),
+            collider.clone(),
+        ));
+        commands.spawn((
+            Transform::from_xyz(-40.0, step * stair_step, step * -2.0 + 20.0),
+            cuboid.clone(),
+            material.clone(),
+            collider.clone(),
+        ));
+        commands.spawn((
+            Transform::from_xyz(step * 2.0 - 20.0, step * stair_step, 40.0),
+            cuboid.clone(),
+            material.clone(),
+            collider.clone(),
+        ));
+        commands.spawn((
+            Transform::from_xyz(step * -2.0 + 20.0, step * stair_step, -40.0),
+            cuboid.clone(),
+            material.clone(),
+            collider.clone(),
+        ));
+    }
+
+    // Light
+    commands.spawn((
+        Transform::from_rotation(Quat::from_euler(EulerRot::ZYX, 0.0, 1.0, -PI / 4.)),
+        DirectionalLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        CascadeShadowConfigBuilder {
+            num_cascades: 2,
+            first_cascade_far_bound: 200.0,
+            maximum_distance: 280.0,
+            ..default()
+        }
+        .build(),
+    ));
+}
+
+
+
+
+
+
+
+
+
+/// ******************************************************************************
+/// ***  UI = CAM2 in top right corner  ******************************************
+/// ******************************************************************************
+
+
+// set up the UI GFY
+pub fn setup_ui(
+    mut commands: Commands,
+    // asset_server: Res<AssetServer>,
+    // mut meshes: ResMut<Assets<Mesh>>,
+    // mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Cameras and their dedicated UI
+    let camera = commands
+        .spawn((
+            Camera3d::default(),
+            Transform::from_translation(Vec3::new(0.0, 40.0, -60.0))
+                .looking_at(Vec3::ZERO, Vec3::Y),
+            Camera {
+                // Renders cameras with different priorities to prevent ambiguities
+                order: 1 as isize,
+                ..default()
+            },
+            TopLeftCamera,
+            CameraPosition {
+                pos: UVec2::new((0 % 2) as u32, (0 / 2) as u32),
+            },
+        ))
+        .id();
+
+    // Set up UI (Little rectangle at the top left corner)
+    // Wrapper of both button PLUS TEXT that have the same portion as it's parent
+    // dynamicly set IG 
+    // It's invisible and just help to set the position of the 2 buttons
+    commands
+        .spawn((
+            UiTargetCamera(camera),
+            // if yo udon't put the node element,
+            // WARN bevy_ecs::hierarchy: warning[B0004]: 
+            // Entity 134v1 with the GlobalTransform component has a parent without GlobalTransform
+            // WARN bevy_ecs::hierarchy: warning[B0004]: 
+            // Entity 134v1 with the InheritedVisibility component has a parent without InheritedVisibility.
+            Node {
+                // 100% is 100% of the rectangle
+                width: Val::Percent(100.),
+                height: Val::Percent(100.),
+                ..default()
+            },
+        ))
+        .with_children(|parent| {
+            parent.spawn((
+                Text::new("Player 1"),
+                Node {
+                    position_type: PositionType::Absolute,
+                    top: Val::Px(12.),
+                    left: Val::Px(12.),
+                    // height and width are dynamicly set by
+                    // set_camera_viewports function
+                    ..default()
+                },
+            ));
+            buttons_panel_spawner(parent);
+        });
+
+    // Wrapper of both button that have the same portion as it's parent
+    // It's invisible and just help to set the position of the 2 buttons
+    fn buttons_panel_spawner(parent: &mut ChildSpawnerCommands) {
+        parent
+            .spawn(Node {
+                position_type: PositionType::Absolute,
+                width: Val::Percent(100.),
+                height: Val::Percent(100.),
+                display: Display::Flex,
+                flex_direction: FlexDirection::Row,
+                justify_content: JustifyContent::SpaceBetween,
+                align_items: AlignItems::Center,
+                padding: UiRect::all(Val::Px(20.)),
+                ..default()
+            })
+            .with_children(|parent| {
+                rotate_button_spawner(parent, "<", Direction::Left);
+                rotate_button_spawner(parent, ">", Direction::Right);
+            });
+    }
+
+    fn rotate_button_spawner(parent: &mut ChildSpawnerCommands, caption: &str, direction: Direction) {
+        parent
+            .spawn((
+                RotateCamera(direction),
+                Button,
+                Node {
+                    width: Val::Px(40.),
+                    height: Val::Px(40.),
+                    border: UiRect::all(Val::Px(2.)),
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    ..default()
+                },
+                BorderColor(Color::WHITE),
+                BackgroundColor(Color::srgb(0.25, 0.25, 0.25)),
+            ))
+            .with_children(|parent| {
+                parent.spawn(Text::new(caption));
+            });
+    }
+}
+
+
